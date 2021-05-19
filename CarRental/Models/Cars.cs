c544088411dff721f7bab84351734c0caf8facba
Данила Сар <mr.sda2014@yandex.ru>
2021-05-19 14:22:21 +0300

    Car

---
+++ CarRental/Models/Cars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental
+{
+    public class Cars
+    {
+        public long Vehicle_code { get; set; }
+        public long Registration_number { get; set; }
+        public long Return_mark { get; set; }
+        public long Date_of_last_maintenance { get; set; }
+        public long Special_marks { get; set; }
+        public long Rental_day_price { get; set; }
+        public long Car_price { get; set; }
+        public long Mileage { get; set; }
+        public long Body_number { get; set; }
+        public long Engine_number { get; set; }
+        public long Year_of_release { get; set; }
+        public long Brand_code { get; set; }
+        public long Employee_Code { get; set; }
+    }
+}
