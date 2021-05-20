32d9108bd7a74f9b57742dc2b311329822cba493
Данила Сар <mr.sda2014@yandex.ru>
2021-05-20 13:07:35 +0300

    Prokat

--- CarRental/Models/Additional services.cs
+++ CarRental/Models/Additional services.cs
@@ -8,10 +8,10 @@ namespace CarRental.Models
 {
     public class Additional_services
     {
-        public long ID { get; set }
-        public int Price { get; set }
-        public string Description { get; set }
-        public string Title { get; set }
+        public long ID { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
         public long Service_code_1 {get; set; }
         public long Service_code_2 { get; set; }
         public long Service_code_3 { get; set; }
