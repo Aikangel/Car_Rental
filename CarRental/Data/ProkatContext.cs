94b1eed18cb882d732e8a232e656cbaf82548ba4
Данила Сар <mr.sda2014@yandex.ru>
2021-05-20 13:39:16 +0300

    Cars

---
+++ CarRental/Data/ProkatContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CarRental.Models;
+
+namespace CarRental.Data
+{
+    public class ProkatContext : DbContext
+    {
+        public ProkatContext (DbContextOptions<ProkatContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CarRental.Models.CarBrands> CarBrands { get; set; }
+    }
+}
--- CarRental/Models/CarBrands.cs
+++ CarRental/Models/CarBrands.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarRental.Models
 {
-    public class CarBrands
+    public class Car_Brands
     {
         public long ID { get; set; }
         public string Description { get; set; }
